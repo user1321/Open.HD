1a0e22d98d880cedb0dd7c78a3b06b815fbe056a
user1321 <33780612+user1321@users.noreply.github.com>
2020-01-27 20:44:24 +0200

    Downlink lora telemetry + switch to openhd part7

--- wifibroadcast-rc-Ath9k/rctxUDP.sh
+++ wifibroadcast-rc-Ath9k/rctxUDP.sh
@@ -5,12 +5,12 @@ ChannelIPCamera=$2
 IsBandSwitcherEnabled=$3
 IsIPCameraSwitcherEnabled=$4
 IsEncrypt=$5
-
-WlanName=$6
+ChannelToSwitchDownlink=$6
+WlanName=$7
 
 while true; do
 
-    nice -n -5 /home/pi/wifibroadcast-rc-Ath9k/rctxUDP_IN $ChannelBandSwitcher $ChannelIPCamera $IsBandSwitcherEnabled $IsIPCameraSwitcherEnabled $IsEncrypt  $WlanName
+    nice -n -5 /home/pi/wifibroadcast-rc-Ath9k/rctxUDP_IN $ChannelBandSwitcher $ChannelIPCamera $IsBandSwitcherEnabled $IsIPCameraSwitcherEnabled $IsEncrypt  $ChannelToSwitchDownlink $WlanName
     sleep 1
 
 done
